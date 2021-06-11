8180bc9168ba1396f792eac30e04baf4ba902a2a
LapisLiozuli <70147943+LapisLiozuli@users.noreply.github.com>
2021-06-12 01:34:39 +0800

    Revert "Added a few items. Added en_us translation. Rabbits now drop Bunny Berries every 10s as a prototype of an upcoming mechanic."
    This reverts commit 0f215432f383f13cd62b638f32f824bdd8fc1c36.

--- src/main/java/com/lapisliozuli/bunnyloversmc/BunnyLovers.java
+++ src/main/java/com/lapisliozuli/bunnyloversmc/BunnyLovers.java
@@ -1,22 +1,9 @@
 package com.lapisliozuli.bunnyloversmc;
 
-import com.lapisliozuli.bunnyloversmc.items.BunnyItems;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.util.Identifier;
 
 public class BunnyLovers implements ModInitializer {
-	public static final String MOD_ID = "bunnyloversmc";
-	public static final ItemGroup BUNNYLOVERSMC = FabricItemGroupBuilder.create(
-			new Identifier(MOD_ID, "bunnyloversmc"))
-			.icon(() -> new ItemStack(Items.CARROT))
-			.build();
-
 	@Override
 	public void onInitialize() {
-		BunnyItems.registerBunnyItems();
 	}
 }
