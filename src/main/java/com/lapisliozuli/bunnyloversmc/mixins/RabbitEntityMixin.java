6e516a9a29cbd4c009d44b4eb244f823388dfe90
LapisLiozuli <70147943+LapisLiozuli@users.noreply.github.com>
2021-06-12 01:34:33 +0800

    Revert "Rabbits take fall damage again?"
    This reverts commit 9be5d7f2d94d97f5f06c7e2ff741ab30ce87d3d7.

--- src/main/java/com/lapisliozuli/bunnyloversmc/mixins/RabbitEntityMixin.java
+++ src/main/java/com/lapisliozuli/bunnyloversmc/mixins/RabbitEntityMixin.java
@@ -12,38 +12,23 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-// This actually targets a static inner class within the RabbitEntity class.
-@Mixin(targets = "net.minecraft.entity.passive.RabbitEntity$EatCarrotCropGoal")
-public class RabbitEntityMixin {
+@Mixin(RabbitEntity.class)
+public abstract class RabbitEntityMixin extends AnimalEntity{
 	public int berryDropTime;
 
+	protected RabbitEntityMixin(EntityType<? extends AnimalEntity> entityType, World world) {
+		super(entityType, world);
+	}
 
-	public void bunnyloversmc$setBerryDropTime(EntityType<? extends RabbitEntity> entityType, World world, CallbackInfo ci) {
+	public boolean bunnyloversmc$handleFallDamage(float fallDistance, float damageMultiplier) {
+		return false;
+	}
+	public void bunnyloversmc$setBerryDropTime(EntityType<? extends RabbitEntity> entityType, World world, CallbackInfo ci){
 		// 5-10 min interval i.e. 20 ticks per second.
 //		this.eggLayTime = this.random.nextInt(6000) + 6000;
 		this.berryDropTime = 200;
 	}
-}
-
-//	@Inject(method = "tick",
-////			at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/state/State;get(Lnet/minecraft/state/property/Property;)Ljava/lang/Comparable;", shift = At.Shift.AFTER)
-////			at = @At(value = "INVOKE", target = "Lnet/minecraft/world/WorldAccess;syncWorldEvent(ILnet/minecraft/util/math/BlockPos;I)V", shift = At.Shift.AFTER)
-//			at = @At(value = "INVOKE",
-//					target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z",
-//					ordinal = 2,
-//					shift = At.Shift.BY,
-//					by = 2),
-//			locals = LocalCapture.PRINT
-//	)
-//	public void bunnyloversmc$tick(CallbackInfo ci) {
-//		// Need to access outer class from inner class.
-////		if (!this.world.isClient && this.isAlive() && !this.isBaby() && --this.berryDropTime <= 0) {
-////			this.playSound(SoundEvents.ENTITY_CHICKEN_EGG, 1.0F, (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
-////			this.dropItem(BunnyItems.BUNNY_BERRIES);
-//	}
-
 
 //	@Inject(at = @At("HEAD"), method = "RabbitEntity")
 //	public void bunnyloversmc$RabbitEntity(EntityType<? extends RabbitEntity> entityType, World world, CallbackInfo ci){
@@ -52,13 +37,14 @@ public class RabbitEntityMixin {
 //		this.berryDropTime = 200;
 //	}
 
-//	@Inject(at = @At("HEAD"), method = "tickMovement")
-//	public void bunnyloversmc$tickMovement(CallbackInfo ci) {
-//		if (!this.world.isClient && this.isAlive() && !this.isBaby() && --this.berryDropTime <= 0) {
-//			this.playSound(SoundEvents.ENTITY_CHICKEN_EGG, 1.0F, (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
-//			this.dropItem(BunnyItems.BUNNY_BERRIES);
-////			this.berryDropTime = this.random.nextInt(6000) + 6000;
-//			this.berryDropTime = 200;
-//		}
-//	}
-//}
+	@Inject(at = @At("HEAD"), method = "tickMovement")
+	public void bunnyloversmc$tickMovement(CallbackInfo ci) {
+		if (!this.world.isClient && this.isAlive() && !this.isBaby() && --this.berryDropTime <= 0) {
+			this.playSound(SoundEvents.ENTITY_CHICKEN_EGG, 1.0F, (this.random.nextFloat() - this.random.nextFloat()) * 0.2F + 1.0F);
+			this.dropItem(BunnyItems.BUNNY_BERRIES);
+//			this.berryDropTime = this.random.nextInt(6000) + 6000;
+			this.berryDropTime = 200;
+		}
+
+	}
+}
