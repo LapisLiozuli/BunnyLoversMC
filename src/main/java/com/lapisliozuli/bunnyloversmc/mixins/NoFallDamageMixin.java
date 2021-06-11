3892161da25ef098140def2bd06bee40e9a4b129
LapisLiozuli <70147943+LapisLiozuli@users.noreply.github.com>
2021-06-11 21:32:17 +0800

    One single mixin and the core of the mod is complete. Time to publish.

--- src/main/java/com/lapisliozuli/bunnyloversmc/mixins/NoFallDamageMixin.java
+++ src/main/java/com/lapisliozuli/bunnyloversmc/mixins/NoFallDamageMixin.java
@@ -1,15 +1,16 @@
 package com.lapisliozuli.bunnyloversmc.mixins;
 
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.entity.passive.RabbitEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(TitleScreen.class)
-public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "init()V")
-	private void init(CallbackInfo info) {
-		System.out.println("This line is printed by an example mod mixin!");
+@Mixin(RabbitEntity.class)
+public class NoFallDamageMixin {
+//	@Inject(at = @At("HEAD"), method = "init()V")
+	public boolean handleFallDamage(float fallDistance, float damageMultiplier) {
+		return false;
 	}
 }
