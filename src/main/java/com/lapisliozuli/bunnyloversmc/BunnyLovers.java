8a9c24d3f31a293ee16083b8d0f0f9f21c21c39f
LapisLiozuli <70147943+LapisLiozuli@users.noreply.github.com>
2021-06-11 21:08:09 +0800

    Changed file and directory names. Got runClient working.

---
+++ src/main/java/com/lapisliozuli/bunnyloversmc/BunnyLovers.java
@@ -0,0 +1,9 @@
+package com.lapisliozuli.bunnyloversmc;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BunnyLovers implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/lapisliozuli/bunnyloversmc/BunnyLoversClient.java
@@ -0,0 +1,15 @@
+package com.lapisliozuli.bunnyloversmc;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class BunnyLoversClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+//		SlimeRenderHandler.registerSlimeEntityColoured();
+//		ColouredSlimeBlocks.renderSlimeBlocks();
+//		SlimyGlass.renderSlimyGlass();
+	}
+}
